3983629a3efaab084959ebad0ecaa91e0fb349d9
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2011-10-08 16:57:15 -0300

    Initial df_fs_check()

--- file.c
+++ file.c
@@ -15,6 +15,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <sys/stat.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,6 +27,8 @@
 void __dead	 usage(void);
 struct df_file	*df_open(const char *);
 void		 df_state_init(int, char **);
+void		 df_check_fs(struct df_file *);
+
 
 extern char	*__progname;
 struct df_state df_state;
@@ -42,14 +46,13 @@ df_state_init(int argc, char **argv)
 {
 	struct df_file *df;
 	int i;
-	
+
 	TAILQ_INIT(&df_state.df_files);
 	for (i = 0; i < argc; i++) {
 		if ((df = df_open(argv[i])) == NULL)
 			err(1, "df_open");
 		TAILQ_INSERT_TAIL(&df_state.df_files, df, entry);
 	}
-	
 }
 
 /* Lib entry point */
@@ -72,20 +75,43 @@ df_open(const char *filename)
 		return (NULL);
 	}
 	/* XXX magic ? */
-	
+
 	return (df);
+}
+
+void
+df_check_fs(struct df_file *df)
+{
+	struct stat sb;
+
+	if (stat(df->filename, &sb) == -1)
+		err(1, "stat: %s", df->filename);
+
+	if (sb.st_mode & S_ISUID)
+		printf("setuid ");
+	if (sb.st_mode & S_ISGID)
+		printf("setgid ");
+	if (sb.st_mode & S_ISVTX)
+		printf("sticky ");
 }
 
 int
 main(int argc, char **argv)
 {
+	struct df_file *df;
+	
 	if (argc < 2)
 		usage();
 
 	argc--;
 	argv++;
 	df_state_init(argc, argv);
-	
-	return (EXIT_SUCCESS);	
-}
 
+	TAILQ_FOREACH(df, &df_state.df_files, entry) {
+		df_check_fs(df);
+		/* df_check_magic(df); */
+		/* df_check_mime(df); */
+	}
+
+	return (EXIT_SUCCESS);
+}
